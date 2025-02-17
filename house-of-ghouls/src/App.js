ef30ec5fa18c8b7e5fa006c44d98e2f34e378214
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2025-02-17 11:15:21 +0000

    added tertiary conditions to navbar

--- house-of-ghouls/src/App.js
+++ house-of-ghouls/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from './Components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- house-of-ghouls/src/Components/NavBar.js
+++ house-of-ghouls/src/Components/NavBar.js
@@ -4,45 +4,41 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
 
 function NavBar() {
+  const [userName,setUsername] = useState();
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
+      <Container fluid className="d-flex justify-content-between align-items-center">
+        {/* Left Section - Navigation Links */}
+        <Nav className="me-auto">
+          <NavDropdown title="Navs" id="navbarScrollingDropdown">
+            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action5">Something else</NavDropdown.Item>
+          </NavDropdown>
+
+          
+            <Nav.Link href={userName ? "#profile" : "#signin"}>
+              {userName ? `Welcome, ${userName}` : "Sign In"}
             </Nav.Link>
-          </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
+        </Nav>
+
+        {/* Center Section - Brand Name */}
+        <Navbar.Brand href="#" className="mx-auto fs-4 fw-bold">House Of Ghouls</Navbar.Brand>
+
+        {/* Right Section - Search Bar */}
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
       </Container>
     </Navbar>
   );
